076f6eef3a74a1ed9650422cbb5dd96a280b08e6
Jose Miralles <josemiralles1991@gmail.com>
2021-06-08 16:30:42 -0400

    wageCoefficient calculated correctly

--- DataLoaders.js
+++ DataLoaders.js
@@ -11,15 +11,28 @@ const getWorkforceWagesArray = () => {
     let xml_string = fs.readFileSync("./data/US_St_Cn_Table_Workforce_Wages.xml", "utf8");
 
     const res = [];
+    let highestWage = Number.MIN_VALUE;
+    let lowestWage = Number.MAX_VALUE;
 
+    // Parse xml document and create data points.
     parser.parseString(xml_string, function (error, result) {
         if (error === null) {
             result["state-county-wage-data"].record.forEach(x => {
+
+                // Check if this datapoint is for a service industry entry.
                 if (x.Industry[0].startsWith("102 ")) {
-                    res.push({
+
+                    // Create a new datapoint with the relevant data.
+                    const dataPoint = {
                         countyCode: x.Cnty[0],
                         averageWeeklyWage: parseInt(x["Annual_Average_Weekly_Wage"][0].replace("_", ""))
-                    });
+                    };
+
+                    // Update highest and lowest wage.
+                    if (highestWage < dataPoint.averageWeeklyWage) highestWage = dataPoint.averageWeeklyWage;
+                    if (lowestWage > dataPoint.averageWeeklyWage) lowestWage = dataPoint.averageWeeklyWage;
+
+                    res.push(dataPoint);
                 };
             });
         } else {
@@ -27,10 +40,20 @@ const getWorkforceWagesArray = () => {
         }
     });
 
+    const difference = highestWage - lowestWage;
+
+    // Iterate trough the list, and assign a coefficient to each datapoint.
+    res.forEach((x, i) => {
+        let coefficient = 0.0
+        coefficient = x.averageWeeklyWage - lowestWage;
+        coefficient = 100 - (coefficient / difference * 100);
+        res[i].wageCoefficient = coefficient;
+    });
+
     return res;
 };
 
-const filtered = getWorkforceWagesArray();
+let filtered = getWorkforceWagesArray();
 filtered.forEach(x => console.log(x));
 
 
